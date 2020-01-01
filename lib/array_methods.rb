0667c0bd378e5095e560586702f95b65a656322c
Diana Wendt <dianakw@gmail.com>
2020-01-01 13:54:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,9 @@ end
 def find_max_value(array)
   max_value = 0
   array.length.times { |index|
-    if array[index]
+    if array[index] > max_value
+      max_value = array[index]
+    end
 end
 
 def find_min_value(array)
